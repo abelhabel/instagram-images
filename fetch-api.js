ccb87ab704c43572114dedbb3e4d40531552d724
abelhabel <anteolsson@gmail.com>
2015-08-05 16:17:02 +0000

    some enhancements

--- fetch-api.js
+++ fetch-api.js
@@ -9,14 +9,19 @@ $.ajax("https://api.instagram.com/v1/tags/lighthouse/media/recent?client_id=d113
 
 function dealWithResponse(response){
   var arr = response.data;
+  console.log(response)
   var container = document.getElementById('img_container');
-  container.style.backgroundImage = "url('')";
+  var caption = document.getElementById('caption');
+  var likes = document.getElementById('likes');
+  var link = document.getElementById('link');
   for(var i = 0; i < arr.length; i += 1){
     (function(i){
-      console.log(i);
       setTimeout(function(){
-        console.log('now', "url('" + arr[i].images.standard_resolution.url + "'')")
         container.style.backgroundImage = "url('" + arr[i].images.standard_resolution.url + "')" || "url()";
+        caption.textContent = arr[i].caption.text;
+        likes.textContent = "Likes: " + arr[i].likes.count;
+        link.textContent = arr[i].link;
+        link.href = arr[i].link;
       },2000 * i + 1000);
     })(i);
   }
